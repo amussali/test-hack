d8d8766ed875ecc556b79b864ee6419e962c7090
Regulus Corneas <kairat.seksembaev@gmail.com>
2026-02-26 00:41:20 +0800

    feat: funny feature 1

---
+++ funny.py
@@ -0,0 +1,15 @@
+class Funny:
+    def __init__(self):
+        self.name = "Funny"
+
+    def joke(self):
+        return "I'm a funny class"
+
+    def laugh(self):
+        return "Haha"
+
+    def cry(self):
+        return "Oh no"
+
+    def sleep(self):
+        return "ZZZZ"
--- main.py
+++ main.py
@@ -8,15 +8,9 @@ def main():
 class SimpleNN:
     def __init__(self, input_size, hidden_size, output_size):
         # Xavier initialization for weights
-        self.W1 = np.random.randn(
-            input_size, hidden_size
-        ) * np.sqrt(
-            1. / input_size
-        )
+        self.W1 = np.random.randn(input_size, hidden_size) * np.sqrt(1.0 / input_size)
         self.b1 = np.zeros((1, hidden_size))
-        self.W2 = np.random.randn(hidden_size, output_size) * np.sqrt(
-            1. / hidden_size
-        )
+        self.W2 = np.random.randn(hidden_size, output_size) * np.sqrt(1.0 / hidden_size)
         self.b2 = np.zeros((1, output_size))
 
     def sigmoid(self, x):
